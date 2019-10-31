5f3bdef68c79a2baf6eaeeb5394d18e215f1a2e4
Carlos Ramirez <20477@columbushs.com>
2019-10-31 09:09:06 -0400

    to finish

--- starter-code/src/functions-and-arrays.js
+++ starter-code/src/functions-and-arrays.js
@@ -1,11 +1,66 @@
 // Iteration #1: Find the maximum
 
+function maxOfTwoNumbers(num1 , num2){
+  if(num1 > num2){
+    return num1;
+  }
+  else{
+    return num2;
+  }
+}
+
 // Iteration #2: Find longest word
-const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+const blah = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+
+function findLongestWord(word){
+
+  let longest = word[0];
+
+  if (word.length  == 0){
+    return null;
+  } 
+
+  // word.forEach( aSingleWord => {
+  //   if(aSingleWord.length > longest.length){
+  //     longest = aSingleWord;
+  //   }
+  // })
+
+  word.forEach( function(aSingleWord){
+    if(aSingleWord.length > longest.length){
+      longest = aSingleWord;
+    }
+  })
+
+  return longest;
+
+}
+
+// findLongestWords(blah)
 
 // Iteration #3: Calculate the sum
-
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumArray(total){
+
+  let sum = 0;
+
+  if (total.length == 0){
+    return 0;
+  }
+
+  total.forEach( function(oneNumber){
+    if(oneNumber.lenght);
+  })
+
+
+
+
+
+
+
+}
+
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
